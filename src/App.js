7ddcf3e6e0c06ec9f6a299926cd5061a68d715ea
Pedro Lucas <pedrolucas122737@gmail.com>
2022-11-18 23:27:10 -0300

    new states and game screen css

--- src/App.js
+++ src/App.js
@@ -22,13 +22,62 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
-  console.log(words)
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
 
+  const picWordAndCategory = () => {
+    // PICK A RANDOM CATEGORY 
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * categories.length)]
+
+    console.log(category)
+
+    // PICK A RANDOM WORD
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+    
+    return {word, category}
+  }
+  
+  // START GAME
+  const startGame = () => {
+    // PICK WORD AND PICK CATEGORY
+    const {word, category} = picWordAndCategory()
+
+    // CREATE AN ARRAY OF LETTERS
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+    
+    console.log(word, category)
+    console.log(wordLetters)
+
+    // FILL STATES 
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+    
+    
+    setGameStage(stages[1].name)
+  }
+
+  // PROCESS THE LETTER INPUT 
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // RESTARTS THE GAME
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+  
   return (
     <div className="App">
-        {gameStage === 'start' && <StartScreen/>}
-        {gameStage === 'game' && <Game/>}
-        {gameStage === 'end' && <GameOver/>}
+        {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+        {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+        {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,32 @@
 import './Game.css'
 
-const Game = () => {
+const Game = ({verifyLetter}) => {
   return (
-    <div>Game</div>
+    <div className='game'>
+      <p className='points'>
+        <span>Pontuação: 0</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className='tip'>
+        Dica <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra: </p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
+    </div>
   )
 }
 
